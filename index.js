51dc46746c7803767b23ac6d1ae7d4b63c7f59cc
Adib Hoque <adibhoque04@gmail.com>
2021-03-29 16:04:53 +0600

    Update index.js

--- index.js
+++ index.js
@@ -10,30 +10,7 @@ app.get("/", (request, response) => {
 app.listen(process.env.PORT);
 
 setInterval(() => {
-  const ty = [
-    'hentai',
-    'gonewild',
-    'thigh',
-    'pussy',
-    'ass',
-    '4k',
-    'pgif', 
-    'anal',
-    'hanal'
-    ]
-  var types = ty[Math.round(Math.random() * (ty.length - 1))];
-  let options = {
-url: "https://nekobot.xyz/api/image?type="+types,
-json: true
-}
-get(options).then(async body => {
-const p = body.message.replace("https://cdn.nekobot.xyz/","https://electro-bot.glitch.me/api/img/")
-const c1 = client.channels.cache.get("738741744602054657")
-const c2 = client.channels.cache.get("738787404931923989")
-if(c1) c1.send(p)
-if(c2) c2.send(p)
-}
-    )
+  
 }, 30000);
 
 setInterval(() => {
