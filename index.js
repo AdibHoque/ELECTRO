b30a88e6d7377722ede96e950d128d0514edc1e9
Adib Hoque <adibhoque04@gmail.com>
2021-04-06 22:05:40 +0600

    Update index.js

--- index.js
+++ index.js
@@ -14,29 +14,7 @@ setInterval(() => {
 }, 30000);
 
 setInterval(() => {
-  const ty = [
-    'cum_jpg',
-    'pussy_jpg',
-    'pwankg',
-    'tits',
-    'boobs',
-    'bj',
-    'cum', 
-    'blowjob'
-    ]
-  var types = ty[Math.round(Math.random() * (ty.length - 1))];
-  let options = {
-url: "https://nekos.life/api/v2/img/"+types,
-json: true
-}
-get(options).then(async body => {
-const p = body.url.replace("https://cdn.nekos.life/","https://electro-bot.glitch.me/api/img/")
-const c1 = client.channels.cache.get("738741744602054657")
-const c2 = client.channels.cache.get("738787404931923989")
-if(c1) c1.send(p)
-if(c2) c2.send(p)
-}
-    )
+  
 }, 60000);
 
 const { Discord, MessageEmbed, Util} = require("discord.js");
