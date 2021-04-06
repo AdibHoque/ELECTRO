8a02aaaa84b1282fbbc24b6bc121fcbb5a5eb85b
Adib Hoque <adibhoque04@gmail.com>
2021-04-06 22:42:49 +0600

    Create guildDelete.js

---
+++ guildDelete.js
@@ -0,0 +1,15 @@
+const Config = require('./../Config.json')
+
+module.exports = async (client, guild) => {
+const channelToSend = await client.channels.fetch(Config.GUILD_ADD_REMOVE_CHANNEL);
+if(!channelToSend) return console.log(`Im in a new Guild named ${guild.name}, you can set a channel to this data to be sent in config.js in GUILD_ADD_REMOVE_CHANNEL variable put the channel ID`)
+ const embed = new MessageEmbed()
+  .setAuthor(`Guild Left`, client.user.avatarURL())
+  .addField(`Guild Name`,guild.name)
+  .addField(`Guild ID`, guild.id)
+  .addField(`Guild Owner`,guild.owner.user.tag)
+  .setThumbnail(guild.iconURL())
+  .setColor(`#ffbf00`)
+  .setFooter(`Total Guilds: ${client.guilds.cache.size} | Users: ${client.users.cache.size}`)
+ channelToSend.send(embed)
+ }
